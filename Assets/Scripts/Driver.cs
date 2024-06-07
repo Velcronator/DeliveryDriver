a65da9bd3acb4631cb4a468cd605a059912236eb
velcronator <valacos7@gmail.com>
2024-06-08 00:58:00 +1000

    Time and basic movement and speed

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,45 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
-    private PlayerInput playerInput;
-    private DeliveryDriver deliveryDriver;
+    [SerializeField] private float steerSpeed = 1f;
+    [SerializeField] private float moveSpeed = 0.01f;
 
-    public float speed = 5f;
-    public float rotationSpeed = 100f;
 
-    private Vector2 moveInput;
-
-    private void Awake()
+    void Update()
     {
-        playerInput = GetComponent<PlayerInput>();
-        deliveryDriver = new DeliveryDriver();
-        deliveryDriver.Player.Enable();
-
-
-        deliveryDriver.Player.Move.performed += DeliveryDriver_Movement;
-    }
-
-    private void FixedUpdate()
-    {
-        // Check if the car is moving
-        if (moveInput.y != 0)
-        {
-            // Apply rotation only when the car is moving
-            float rotation = -moveInput.x * rotationSpeed * Time.deltaTime;
-            transform.Rotate(Vector3.forward, rotation);
-        }
-
-        // Apply movement
-        Vector3 movement = transform.up * moveInput.y * speed * Time.deltaTime;
-        transform.position += movement;
-    }
-
-    public void DeliveryDriver_Movement(InputAction.CallbackContext callbackContext)
-    {
-        moveInput = callbackContext.ReadValue<Vector2>();
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        // get the vertical axis input
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
